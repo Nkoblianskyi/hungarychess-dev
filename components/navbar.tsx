b14832547ab56acf8b18984d539008ce8a12d048
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2026-05-01 11:59:51 +0200

    1

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { useState } from "react"
+import Image from "next/image"
 
 const navLinks = [
   { href: "/", label: "Főoldal" },
